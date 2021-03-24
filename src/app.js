a4bea4bbf6b0ba4bc89cc2afd2c73cc654ee156b
Kate Pilbean <katepilbean@gmail.com>
2021-03-24 12:12:19 -0400

    Separated DB files to folder, added SQL-DB coding to JS, added gitignore.

--- src/app.js
+++ src/app.js
@@ -7,17 +7,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/contact-requests");
-const contactSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    emailAddress: String,
-    message: String
-});
-const Request = mongoose.model("Request", contactSchema);
-
 const publicFolder = path.join(__dirname, '../public');
 app.use(express.static(publicFolder));
 
@@ -34,6 +23,102 @@ app.get('/', function (req, res) {
 //     res.send("<h1>Hello from the About page.</h1>");
 // });
 
+
+// SQLite Databases (Education, Projects, & Experience Sections) & Tests
+const sqlite3 = require('sqlite3').verbose();
+
+let EducationDB = new sqlite3.Database('../SQL-DB/EducationDB.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    } else {
+        console.log('Connected to the EducationDB SQLite database.');
+    }
+});
+
+EducationDB.serialize(() => {
+    EducationDB.run('CREATE TABLE Education')
+    EducationDB.each(`SELECT SchoolId as id, SchoolName as name FROM Schools`, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log(row.id + "\t" + row.name);
+        }
+    });
+});
+
+EducationDB.close((err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('Close the EducationDB connection.')
+    }
+});
+
+let ProjectsDB = new sqlite3.Database('../SQL-DB/ProjectsDB.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        return console.error(err.message);
+    } else {
+        console.log('Connected to the ProjectsDB SQLite database.');
+    }
+});
+
+ProjectsDB.serialize(() => {
+    ProjectsDB.each(`SELECT ProjectId as id, ProjectName as name FROM Projects`, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log(row.id + "\t" + row.name);
+        }
+    });
+});
+
+ProjectsDB.close((err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('Close the ProjectsDB connection.')
+    }
+});
+
+let ExperienceDB = new sqlite3.Database('../SQL-DB/ExperienceDB.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        return console.error(err.message);
+    } else {
+        console.log('Connected to the ExperienceDB SQLite database.');
+    }
+});
+
+ExperienceDB.serialize(() => {
+    ExperienceDB.each(`SELECT ExperienceId as id, ExperienceName as name FROM Experience`, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log(row.id + "\t" + row.name);
+        }
+    });
+});
+
+ExperienceDB.close((err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('Close the ExperienceDB connection.')
+    }
+});
+
+
+// MongoDB Contact Request Form & Test
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/contact-requests");
+const contactSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    emailAddress: String,
+    message: String
+});
+const Request = mongoose.model("Request", contactSchema);
+
 app.post("/contactMe", (req, res) => {
     const userInput = new Request(req.body);
     userInput.save()
@@ -44,6 +129,7 @@ app.post("/contactMe", (req, res) => {
             res.send(400).send("I'm sorry, your submission was unsuccessful.");
         });
 });
+
 
 app.listen(port, () => {
     console.log("The server is running on port " + port + ".");
